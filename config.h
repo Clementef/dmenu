65fb5229c4caa8440dcebb29e9847c901791b19d
clementef <clementef2000@gmail.com>
2024-02-18 13:39:17 -0800

    lines below prompt and dracula patches

--- config.h
+++ config.h
@@ -9,11 +9,11 @@ static int min_width = 500;                    /* minimum width when centered */
 static const char *fonts[] = {
 	"monospace:size=10"
 };
-static const char *prompt      = "dmenu";      /* -p  option; prompt to the left of input field */
+static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
+	[SchemeNorm] = { "#f8f8f2", "#282a36" },
+	[SchemeSel] = { "#f8f8f2", "#6272a4" },
 	[SchemeSelHighlight] = { "#ffc978", "#005577" },
 	[SchemeNormHighlight] = { "#ffc978", "#222222" },
 	[SchemeOut] = { "#000000", "#00ffff" },
@@ -21,7 +21,7 @@ static const char *colors[SchemeLast][2] = {
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 5;
 /* -h option; minimum height of a menu line */
-static unsigned int lineheight = 10;
+static unsigned int lineheight = 0;
 static unsigned int min_lineheight = 8;
 
 /*
@@ -31,4 +31,4 @@ static unsigned int min_lineheight = 8;
 static const char worddelimiters[] = " ";
 
 /* Size of the window border */
-static unsigned int border_width = 1;
+static unsigned int border_width = 0;
